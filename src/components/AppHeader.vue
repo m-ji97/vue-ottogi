989bc5c8d47737a6cd4871cf454d584e1c8ebbff
m-ji97 <tv5035@naver.com>
2024-04-12 17:29:19 +0900

    240412 프로젝트

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div id="header" class="clearfix">
+            
+            <router-link to="/">
+                <img id="logo" src="@/assets/img/로고.png" alt="conan" width="150px" />
+            </router-link>
+            
+        </div>
+        <!-- //header -->
+        <nav id="nav">
+            
+        </nav>
+    </div> 
+</template>
+<script>
+import '@/assets/css/AppHeader.css'
+
+export default {
+    name: "AppHeader",
+    components: {},
+    data() {
+        return {};
+    },
+    methods: {
+    },
+    created() { }
+};
+</script>
